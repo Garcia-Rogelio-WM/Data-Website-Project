6fe2186dac5708c2b943246e0226e401fb821449
Rogelio Garcia <rogelio.garcia@west-mec.org>
2015-10-22 12:54:10 -0700

    Picture dropdown menu 3x3 pictures

---
+++ picture.js
@@ -0,0 +1,94 @@
+
+
+
+
+function run() {
+    if (document.getElementById("pic1").src == "https://pbs.twimg.com/profile_images/378800000822867536/3f5a00acf72df93528b6bb7cd0a4fd0c.jpeg")
+    {
+        document.getElementById("pic1").src = "http://i0.kym-cdn.com/photos/images/newsfeed/000/103/740/Me%20Gusta.png?1318992465";
+    }
+    else
+    {document.getElementById("pic1").src = "https://pbs.twimg.com/profile_images/378800000822867536/3f5a00acf72df93528b6bb7cd0a4fd0c.jpeg"}
+}
+
+
+function run2() {
+    if (document.getElementById("pic2").src == "http://i0.kym-cdn.com/photos/images/newsfeed/000/103/740/Me%20Gusta.png?1318992465") {
+        document.getElementById("pic2").src = "https://avatars2.githubusercontent.com/u/13558379?v=3&s=400";
+    }
+    else {
+        document.getElementById("pic2").src = "http://i0.kym-cdn.com/photos/images/newsfeed/000/103/740/Me%20Gusta.png?1318992465"
+    }
+}
+
+
+function run3() {
+    if (document.getElementById("pic3").src == "http://dreamatico.com/data_images/apple/apple-7.jpg"){
+        document.getElementById("pic3").src = "https://yt3.ggpht.com/-KdgJnz1HIdQ/AAAAAAAAAAI/AAAAAAAAAAA/4vVN7slJqj4/s900-c-k-no/photo.jpg";
+    }
+    else
+    {
+        document.getElementById("pic3").src = "http://dreamatico.com/data_images/apple/apple-7.jpg"
+    }
+}
+
+function run4() {
+    if (document.getElementById("pic4").src == "http://horizonspestcontrol.com/wp-content/uploads/2013/07/House-Mouse.jpg"){
+        document.getElementById("pic4").src = "http://www.afhe.org/images/computer_mouse.jpg"
+    }
+    else
+    {
+        document.getElementById("pic4").src = "http://horizonspestcontrol.com/wp-content/uploads/2013/07/House-Mouse.jpg"
+    }
+}
+
+function run5() {
+    if (document.getElementById("pic5").src == "http://mrmeeseeks.com/box.png")
+    {
+        document.getElementById("pic1").src = "http://40.media.tumblr.com/eaf32fcaed9b1295fdada1e31477e841/tumblr_nwhk5578KU1uhwnn1o1_1280.png"
+        document.getElementById("pic2").src = "http://i.ytimg.com/vi/VC8H5B2YVCY/maxresdefault.jpg";
+        document.getElementById("pic3").src = "https://pbs.twimg.com/profile_images/478458360405049344/6OHQAaFk_400x400.png";
+        document.getElementById("pic4").src = "https://media2.giphy.com/media/WvOjT988OZiNy/200_s.gif";
+        document.getElementById("pic6").src = "http://cdn.akamai.steamstatic.com/steamcommunity/public/images/avatars/7d/7d3d5b39f3606b06451c682dfdbac782be8420d3_full.jpg";
+        document.getElementById("pic7").src = "http://cdn.akamai.steamstatic.com/steamcommunity/public/images/avatars/ce/ceeef2498e2f3a61e85838a323aecb3210e31b50_full.jpg";
+        document.getElementById("pic8").src = "http://media.steampowered.com/steamcommunity/public/images/avatars/61/61a0c2776cde61da7399c8b3dfdf86343744dbb0_full.jpg";
+        document.getElementById("pic9").src = "http://orig00.deviantart.net/245e/f/2014/072/9/f/i_m_mr__meeseeks__by_porthead-d7a15u1.png";
+
+
+
+    }
+    else
+    {document.getElementById("pic5").src = "http://mrmeeseeks.com/box.png"}
+}
+function run6() {
+    if (document.getElementById("pic6").src == "http://4.bp.blogspot.com/-tDBh_PgjRO8/UBffW32f6aI/AAAAAAAABFw/_BNK-o8SsVs/s1600/little+kid+happy.png")
+    {
+        document.getElementById("pic6").src = "http://www.theworldofmeme.com/418394810.png";
+    }
+    else
+    {document.getElementById("pic6").src = "http://4.bp.blogspot.com/-tDBh_PgjRO8/UBffW32f6aI/AAAAAAAABFw/_BNK-o8SsVs/s1600/little+kid+happy.png"}
+}
+function run7() {
+    if (document.getElementById("pic7").src == "http://memesvault.com/wp-content/uploads/Happy-Meme-Faces-26.png")
+    {
+        document.getElementById("pic7").src = "http://funnyfamilywallpaper.com/wp-content/uploads/2015/06/meme-guy-holding-tea-cup-with-feet-6.jpg";
+    }
+    else
+    {document.getElementById("pic7").src = "http://memesvault.com/wp-content/uploads/Happy-Meme-Faces-26.png"}
+}
+function run8() {
+    if (document.getElementById("pic8").src == "http://memesvault.com/wp-content/uploads/Philosoraptor-What-If-10.jpg")
+    {
+        document.getElementById("pic8").src = "http://static.fjcdn.com/comments/Quot+meme+faces+quot+_49e6da7abb3ac3a11eb104d1cbeb6051.png";
+    }
+    else
+    {document.getElementById("pic8").src = "http://memesvault.com/wp-content/uploads/Philosoraptor-What-If-10.jpg"}
+}
+function run9() {
+    if (document.getElementById("pic9").src == "http://cdn.quotesgram.com/img/28/73/1086880110-zoidberg-9883dd4b00ec8dfda9ae15ba87dad0058b4fa2fb-s6-c30.jpg")
+    {
+        document.getElementById("pic9").src = "http://blog.eyewire.org/wp-content/uploads/2013/09/zoidberg.jpg";
+    }
+    else
+    {document.getElementById("pic9").src = "http://cdn.quotesgram.com/img/28/73/1086880110-zoidberg-9883dd4b00ec8dfda9ae15ba87dad0058b4fa2fb-s6-c30.jpg"}
+}
